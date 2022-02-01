cf39a2ed384de5dcd648528d14fb24fa33d4f961
asmRakibulHasan <74839882+asmRakibulHasan@users.noreply.github.com>
2022-02-01 23:33:49 +0600

    Time: 27 ms (57.81%), Space: 9.3 MB (52.15%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,18 +1,18 @@
 class Solution {
-    map<pair<int,int>,int>dp;
-    map<pair<int,int>,bool>dp1;
+    int dp[22][2005];
+    bool dp1[22][2005];
 public:
     
     int call(int idx,int now,vector<int>& nums,int target){
         if(idx==nums.size()) return now==target;
-        if(dp1[{idx,now}]) return dp[{idx,now}];
+        if(dp1[idx][now+1000]) return dp[idx][now+1000];
         
-        dp1[{idx,now}] = true;
+        dp1[idx][now+1000] = true;
         
         int ans = call(idx+1,now-(nums[idx]),nums,target);
         ans += call(idx+1,now+(nums[idx]),nums,target);
         
-        return dp[{idx,now}]=ans;
+        return dp[idx][now+1000]=ans;
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
