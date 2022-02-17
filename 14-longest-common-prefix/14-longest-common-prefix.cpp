ead3a8a0244ea55d2b1283f6f65bcda995a34cc0
asmRakibulHasan <74839882+asmRakibulHasan@users.noreply.github.com>
2022-02-18 04:58:27 +0600

    Time: 4 ms (75.23%), Space: 9.3 MB (27.79%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,43 +1,20 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        int hi = 10000;
+        int xx=10000;
         for(auto it:strs) {
-            if(it.size()<hi) hi = it.size();
+            if(it.size()==0) return "";
+            if(it.size()<xx) xx=it.size();
         }
-        
-        if(!hi) return "";
-        
-        int lo = 1;
-        
-        while(hi>lo) {
-            int mid = (lo+hi+1)>>1;
-            
-            int f = 1;
-            for(int i=1;i<strs.size();i++) {
-                int ff=0;
-                for(int j=0;j<mid;j++) {
-                    if(strs[0][j]!=strs[i][j]) ff=1;
+        string ans="";
+        int n=strs.size();
+        for(int i=0;i<xx;i++) {
+            for(int j=1;j<n;j++) {
+                if(strs[0][i]!=strs[j][i]) {
+                    return ans;
                 }
-                if(ff) f=0;
             }
-            
-            if(f) lo = mid;
-            else hi = mid-1;
-        }
-        
-        int f = 1;
-        for(int i=1;i<strs.size();i++) {
-            int ff=0;
-            for(int j=0;j<lo;j++) {
-                if(strs[0][j]!=strs[i][j]) ff=1;
-            }
-            if(ff) f=0;
-        }
-        
-        string ans;
-        if(f) {
-            for(int i=0;i<lo;i++) ans+=strs[0][i];
+            ans.push_back(strs[0][i]);
         }
         return ans;
     }
