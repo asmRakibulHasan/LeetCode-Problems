bc424f67653ee85b37f22fe9a810e91e7f202ffc
asmRakibulHasan <74839882+asmRakibulHasan@users.noreply.github.com>
2022-03-04 23:31:48 +0600

    Time: 7 ms (50.87%), Space: 9.6 MB (73.72%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.cpp
+++ 35-search-insert-position/35-search-insert-position.cpp
@@ -4,7 +4,7 @@ public:
         int lo = 0, hi = nums.size();
         
         while( hi > lo ) {
-            int mid = (lo+hi)>>1;
+            int mid = lo+((hi-lo)>>1);
             
             if(nums[mid]>=target) hi = mid;
             else lo = mid+1;
